ba79c41549a2f5f26dcfc387b672795c720a3639
Joacim <broboy111@gmail.com>
2018-10-16 20:58:26 +0200

    123
    123

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import wx # GUI Module
-import sys # For timers
+import time # For timers
 
 
 if __name__ == "__main__":  #Will only execute if its the file being executed (can't be accessed by other programs.)
     while True:
         print("hello world")
-        sys.timers
-
-if __name__ != "__main__":
-    print("idk")
+        time.sleep(1)
