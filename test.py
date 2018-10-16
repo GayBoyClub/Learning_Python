823b4dff7231774d7c9e91cbd9cb072e9740762c
Joacim <broboy111@gmail.com>
2018-10-16 21:02:12 +0200

    Update test.py
    Co-Authored-By: nanowhales <nanowhales@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import wx # GUI Module
-import sys # For timers
+import time # For timers
 
 
 if __name__ == "__main__":  #Will only execute if its the file being executed (can't be accessed by other programs.)
     while True:
         print("hello world")
-        sys.timers
-
-if __name__ != "__main__":
-    print("idk")
-=======
-Kill me i wanna die
-d12
->>>>>>> parent of d5e3908... Update test.py
+        time.sleep(1)
+        
