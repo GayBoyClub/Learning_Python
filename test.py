595b3905126ed0598d86e232e2643c45d50023c2
Joacim <broboy111@gmail.com>
2018-10-16 21:00:54 +0200

    Revert "123"
    This reverts commit ba79c41549a2f5f26dcfc387b672795c720a3639.

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import wx # GUI Module
-import time # For timers
+import sys # For timers
 
 
 if __name__ == "__main__":  #Will only execute if its the file being executed (can't be accessed by other programs.)
     while True:
         print("hello world")
-        time.sleep(1)
+        sys.timers
+
+if __name__ != "__main__":
+    print("idk")
